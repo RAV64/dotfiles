7c66a2e3e3fd467278cdd3a8ce19c5946f9003fe
MikiS <73443709+RAV64@users.noreply.github.com>
2023-02-22 15:17:08 +0200

    Syscall chatter hushed delivering incremental serenity.

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -3,5 +3,6 @@ ln -fs ~/dotfiles/nvim ~/.config
 ln -fs ~/dotfiles/bat ~/.config
 ln -fs ~/dotfiles/karabiner ~/.config
 ln -fs ~/dotfiles/fish ~/.config
+ln -fs ~/dotfiles/wezterm ~/.config
 ln -f ~/dotfiles/zsh/.zshrc ~/
 ln -f ~/dotfiles/.gitconfig ~/
