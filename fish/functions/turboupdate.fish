0b1d69562482d3a7596fa366d5342eb602c6970f
MikiS <73443709+RAV64@users.noreply.github.com>
2023-02-27 08:25:24 +0200

    I/O patience cultivated because simple is surprisingly difficult.

--- fish/functions/turboupdate.fish
+++ fish/functions/turboupdate.fish
@@ -5,4 +5,7 @@ rustup update
 npm update -g
 cargo install-update -a
 fisher update
+brew upgrade --cask wez/wezterm/wezterm-nightly --no-quarantine --greedy-latest
+brew upgrade neovim --fetch-HEAD
+bun upgrade
 end
