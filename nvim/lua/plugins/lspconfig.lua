992e36a99110e0a332a66b6045e86d4358e4eafc
MikiS <73443709+RAV64@users.noreply.github.com>
2023-02-27 08:31:28 +0200

    SIMD ambitions tempered that should have been done years ago.

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -7,19 +7,19 @@ return {
 		"SmiteshP/nvim-navic",
 	},
 	keys = {
-		{ "gf",    vim.lsp.buf.format,                        desc = "Format" },
-		{ "K",     vim.lsp.buf.hover,                         desc = "Hover" },
-		{ "gr",    vim.lsp.buf.rename,                        desc = "Rename" },
-		{ "gd",    "<cmd>Telescope lsp_definitions<cr>",      desc = "Goto Definition" },
-		{ "gD",    vim.lsp.buf.declaration,                   desc = "Get Declaration" },
-		{ "gk",    vim.diagnostic.goto_prev,                  desc = "Goto previous diagnostics" },
-		{ "gj",    vim.diagnostic.goto_next,                  desc = "Goto next diagnostics" },
-		{ "ge",    vim.diagnostic.open_float,                 desc = "Open diagnostics" },
-		{ "ga",    vim.lsp.buf.code_action,                   desc = "Get code actions" },
-		{ "<C-s>", vim.lsp.buf.signature_help,                desc = "Show signature",           mode = "i" },
-		{ "gu",    "<cmd>Telescope lsp_references<cr>",       desc = "Get Usages" },
-		{ "gi",    "<cmd>Telescope lsp_implementations<cr>",  desc = "Get implementations" },
-		{ "gt",    "<cmd>Telescope lsp_type_definitions<cr>", desc = "Get type definitions" },
+		{ "gf", vim.lsp.buf.format, desc = "Format" },
+		{ "K", vim.lsp.buf.hover, desc = "Hover" },
+		{ "gr", vim.lsp.buf.rename, desc = "Rename" },
+		{ "gd", "<cmd>Telescope lsp_definitions<cr>", desc = "Goto Definition" },
+		{ "gD", vim.lsp.buf.declaration, desc = "Get Declaration" },
+		{ "gk", vim.diagnostic.goto_prev, desc = "Goto previous diagnostics" },
+		{ "gj", vim.diagnostic.goto_next, desc = "Goto next diagnostics" },
+		{ "ge", vim.diagnostic.open_float, desc = "Open diagnostics" },
+		{ "ga", vim.lsp.buf.code_action, desc = "Get code actions" },
+		{ "<C-s>", vim.lsp.buf.signature_help, desc = "Show signature", mode = "i" },
+		{ "gu", "<cmd>Telescope lsp_references<cr>", desc = "Get Usages" },
+		{ "gi", "<cmd>Telescope lsp_implementations<cr>", desc = "Get implementations" },
+		{ "gt", "<cmd>Telescope lsp_type_definitions<cr>", desc = "Get type definitions" },
 		{
 			"gwa",
 			vim.lsp.buf.add_workspace_folder,
@@ -40,12 +40,8 @@ return {
 	},
 	opts = {
 		servers = {
-			pyright = {
-				settings = {},
-			},
-			bashls = {
-				settings = {},
-			},
+			pyright = {},
+			bashls = {},
 			lua_ls = {
 				settings = {
 					Lua = {
@@ -67,22 +63,16 @@ return {
 				},
 			},
 			tsserver = {
-				settings = {},
+				cmd = { "bun", "run", "typescript-language-server", "--stdio" },
 			},
-			csharp_ls = {
-				settings = {},
-			},
-			zls = {
-				settings = {},
-			},
-			sqlls = {
-				settings = {},
-			},
-			ruff_lsp = {
-				settings = {},
-			},
-			astro = {
-				settings = {},
+			csharp_ls = {},
+			zls = {},
+			sqlls = {},
+			ruff_lsp = {},
+			astro = {},
+			prismals = {},
+			tailwindcss = {
+				cmd = { "bun", "run", "tailwindcss-language-server", "--stdio" },
 			},
 		},
 	},
@@ -96,6 +86,7 @@ return {
 			lspconfig[name].setup({
 				capabilities = capabilities,
 				settings = lsp.settings,
+				cmd = lsp.cmd,
 				on_attach = function(client, bufnr)
 					if client.server_capabilities.documentSymbolProvider then
 						navic.attach(client, bufnr)
