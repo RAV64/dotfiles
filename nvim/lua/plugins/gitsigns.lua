af286c557c09f36ba15c6fbed81cc5a2b8d4b995
MikiS <73443709+RAV64@users.noreply.github.com>
2023-02-27 08:31:42 +0200

    keymappings for gitsign

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,6 +1,13 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	event = { "BufReadPre", "BufNewFile" },
+	keys = {
+		{ "<leader>vb", "<cmd>lua require'gitsigns'.blame_line{full=true}<cr>", desc = "Line blame" },
+		{ "<leader>vn", "<cmd>Gitsigns next_hunk<cr>", desc = "Next hunk" },
+		{ "<leader>vp", "<cmd>Gitsigns prev_hunk<cr>", desc = "Previous hunk" },
+		{ "<leader>vs", "<cmd>Telescope git_status<cr>", desc = "Git status" },
+		{ "<leader>vh", "<cmd>Gitsigns preview_hunk_inline<cr>", desc = "Git status" },
+	},
 	opts = {
 		signs = {
 			add = { text = "▎" },
@@ -10,5 +17,7 @@ return {
 			changedelete = { text = "▎" },
 			untracked = { text = "▎" },
 		},
+		numhl = true,
+		linehl = false,
 	},
 }
