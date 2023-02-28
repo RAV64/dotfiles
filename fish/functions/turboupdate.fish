5c53d5f5a35c3c9e7fa4ba9bea9ea1b6d3072adb
MikiS <73443709+RAV64@users.noreply.github.com>
2023-02-28 10:47:51 +0200

    Update all global bun packages

--- fish/functions/turboupdate.fish
+++ fish/functions/turboupdate.fish
@@ -1,11 +1,13 @@
 function turboupdate
-brew upgrade
-brew update
-rustup update
-npm update -g
-cargo install-update -a
-fisher update
-brew upgrade --cask wez/wezterm/wezterm-nightly --no-quarantine --greedy-latest
-brew upgrade neovim --fetch-HEAD
-bun upgrade
+    brew upgrade
+    brew update
+    rustup update
+    npm update -g
+    cargo install-update -a
+    fisher update
+    brew upgrade --cask wez/wezterm/wezterm-nightly --no-quarantine --greedy-latest
+    brew upgrade neovim --fetch-HEAD
+    bun upgrade
+    bun-update
+    tldr --update
 end
