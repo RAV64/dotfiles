67800ee461f90b54b3d5e84503ae0f180127ddf7
MikiS <73443709+RAV64@users.noreply.github.com>
2023-04-25 13:05:51 +0300

    updating

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,7 @@ if status is-interactive
   export VISUAL="nvim"
 
   set -Ux FZF_DEFAULT_OPTS "\
-  --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+  --color=bg+:#313244,bg:#202020,spinner:#f5e0dc,hl:#f38ba8 \
   --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
   --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
