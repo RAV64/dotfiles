c5bff2516feee2ad9db0e5f18823074ec07ed2ee
MikiS <73443709+RAV64@users.noreply.github.com>
2023-10-16 08:08:32 +0300

    Version vectors synchronized without violating the laws of physics.

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,21 +1,21 @@
 return {
 	"neovim/nvim-lspconfig",
-	event = "BufReadPre",
+	event = { "BufReadPost", "BufNewFile", "BufWritePre" },
 	dependencies = {
 		{ "folke/neodev.nvim", opts = {} },
 		{
 			"SmiteshP/nvim-navic",
-			opts = {
-				lsp = {
-					auto_attach = true,
-				},
-			},
+			opts = function()
+				vim.g.navic_silence = true
+				return {
+					lsp = { auto_attach = true },
+					highlight = true,
+				}
+			end,
 		},
 		{
 			"SmiteshP/nvim-navbuddy",
-			dependencies = {
-				"MunifTanjim/nui.nvim",
-			},
+			dependencies = { "MunifTanjim/nui.nvim" },
 			opts = {
 				lsp = { auto_attach = true },
 				window = {
@@ -35,7 +35,7 @@ return {
 	keys = {
 		{ "K", vim.lsp.buf.hover, desc = "Hover" },
 		{ "gr", vim.lsp.buf.rename, desc = "Rename" },
-		{ "gd", "<cmd>Telescope lsp_definitions<cr>", desc = "Goto Definition" },
+		{ "gd", function() require("telescope.builtin").lsp_definitions({ reuse_win = true }) end, desc = "Goto Definition" },
 		{ "gD", vim.lsp.buf.declaration, desc = "Get Declaration" },
 		{ "gk", vim.diagnostic.goto_prev, desc = "Goto previous diagnostics" },
 		{ "gj", vim.diagnostic.goto_next, desc = "Goto next diagnostics" },
@@ -43,54 +43,31 @@ return {
 		{ "ga", vim.lsp.buf.code_action, desc = "Get code actions" },
 		{ "<C-s>", vim.lsp.buf.signature_help, desc = "Show signature", mode = "i" },
 		{ "gu", "<cmd>Telescope lsp_references<cr>", desc = "Get Usages" },
-		{ "gi", "<cmd>Telescope lsp_implementations<cr>", desc = "Get implementations" },
-		{ "gt", "<cmd>Telescope lsp_type_definitions<cr>", desc = "Get type definitions" },
+		{ "gi", function() require("telescope.builtin").lsp_implementations({ reuse_win = true }) end, desc = "Get implementations" },
+		{ "gt", function() require("telescope.builtin").lsp_type_definitions({ reuse_win = true }) end, desc = "Get type definitions" },
 		{ "gwa", vim.lsp.buf.add_workspace_folder, desc = "add_workspace_folder"},
 		{ "gwr", vim.lsp.buf.remove_workspace_folder, "remove_workspace_folder"},
 		{ "gwl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, "list_workspace_folders"},
 	},
 	opts = {
 		servers = {
-			pyright = {
-				cmd = { "bun", "run", "pyright-langserver", "--stdio" },
-			},
-			bashls = {
-				cmd = { "bun", "run", "bash-language-server", "start" },
-			},
+			pyright = { cmd = { "bun", "run", "pyright-langserver", "--stdio" } },
+			tsserver = { cmd = { "bun", "run", "typescript-language-server", "--stdio" } },
+			bashls = { cmd = { "bun", "run", "bash-language-server", "start" } },
+			-- tailwindcss = { cmd = { "bun", "run", "tailwindcss-language-server", "--stdio" } },
 			lua_ls = {
 				settings = {
 					Lua = {
-						runtime = {
-							version = "LuaJIT",
-						},
-						diagnostics = {
-							globals = {
-								"vim",
-							},
-						},
+						runtime = { version = "LuaJIT" },
+						diagnostics = { globals = { "vim" } },
 						workspace = {
 							library = vim.api.nvim_get_runtime_file("", true),
 							checkThirdParty = false,
 						},
-						telemetry = {
-							enable = false,
-						},
+						telemetry = { enable = false },
 					},
 				},
 			},
-			tsserver = {
-				cmd = { "bun", "run", "typescript-language-server", "--stdio" },
-			},
-			csharp_ls = {},
-			clojure_lsp = {},
-			zls = {},
-			sqlls = {},
-			ruff_lsp = {},
-			astro = {},
-			prismals = {},
-			-- tailwindcss = {
-			-- 	cmd = { "bun", "run", "tailwindcss-language-server", "--stdio" },
-			-- },
 			rust_analyzer = {
 				settings = {
 					["rust-analyzer"] = {
@@ -103,12 +80,17 @@ return {
 							features = "all",
 							extraArgs = { "--no-deps" },
 						},
-						procMacro = {
-							enable = true,
-						},
+						procMacro = { enable = true },
 					},
 				},
 			},
+			csharp_ls = {},
+			clojure_lsp = {},
+			zls = {},
+			sqlls = {},
+			ruff_lsp = {},
+			astro = {},
+			prismals = {},
 		},
 		diagnostics = {
 			underline = true,
@@ -128,10 +110,6 @@ return {
 		format_notify = false,
 	},
 	config = function(_, opts)
-		vim.g.navic_silence = true
-		local navic = require("nvim-navic")
-		navic.setup({ highlight = true })
-
 		local has_cmp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 		local capabilities = vim.tbl_deep_extend(
 			"force",
