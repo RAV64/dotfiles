86c1423ce131708637ec47d05b46b5f88616b012
MikiS <73443709+RAV64@users.noreply.github.com>
2023-10-31 17:46:51 +0200

    Wezzy term

--- wezterm/tabs.lua
+++ wezterm/tabs.lua
@@ -3,24 +3,41 @@ local wezterm = require("wezterm")
 local M = {}
 M.arrow_solid = ""
 M.arrow_thin = ""
-M.icons = {
-	["v"] = wezterm.nerdfonts.custom_vim,
-	["cargo"] = "",
-	["cr"] = "",
-	["ct"] = "",
-	["cw"] = "",
+M.home_dir = os.getenv("HOME")
+
+M.filetype = {
+	["rust"] = { { Foreground = { Color = "#f5a97f" } }, { Text = "" } },
 }
 
-function M.title(tab, max_width)
-	local title = (tab.tab_title and #tab.tab_title > 0) and tab.tab_title or tab.active_pane.title
-	local process, other = title:match("^(%S+)%s*%-?%s*%s*(.*)$")
+M.icons = {
+	["nvim"] = { { Foreground = { Color = "#89e051" } }, { Text = wezterm.nerdfonts.custom_vim } },
+	["git"] = { Foreground = { Color = "#41535b" }, { Text = "󰊢" } },
+	["Python"] = { { Foreground = { Color = "#F7CE57" } }, { Text = "" } },
+	["fish"] = { { Foreground = { Color = "#cdd6f4" } }, { Text = "" } },
+	["cargo-watch"] = M.filetype["rust"],
+	["cargo"] = M.filetype["rust"],
+	["cr"] = M.filetype["rust"],
+	["ct"] = M.filetype["rust"],
+	["mdbook"] = { { Text = "" } },
+	["watch"] = { { Text = "󰜎" } },
+}
+function M.basename(s)
+	return string.gsub(s, "(.*/)(.*)", "%2")
+end
 
-	if M.icons[process] then
-		title = M.icons[process] .. " " .. (other or "")
+function M.process(tab)
+	local process_name = M.basename(tab.active_pane.foreground_process_name)
+	return wezterm.format(M.icons[process_name] or { { Text = string.format("%s:", process_name) } })
+end
+
+function M.cwd(tab)
+	local current_dir = tab.active_pane.current_working_dir.file_path
+
+	if current_dir == M.home_dir then
+		return "~"
 	end
 
-	title = wezterm.truncate_right(title, max_width - 3)
-	return " " .. title .. " "
+	return M.basename(current_dir)
 end
 
 function M.setup(config)
@@ -33,18 +50,21 @@ function M.setup(config)
 	config.show_new_tab_button_in_tab_bar = false
 
 	wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-		local title = M.title(tab, max_width)
 		local colors = config.resolved_palette
 		local active_bg = colors.tab_bar.active_tab.bg_color
 		local inactive_bg = colors.tab_bar.inactive_tab.bg_color
 
-		local tab_idx = 1
+		local tab_idx
 		for i, t in ipairs(tabs) do
 			if t.tab_id == tab.tab_id then
 				tab_idx = i
 				break
 			end
 		end
+
+		local title = #tabs < 7 and string.format(" %s  %s ", M.process(tab), M.cwd(tab))
+			or tab.is_active and string.format(" %s  %s ", M.process(tab), M.cwd(tab))
+			or string.format("%s %s ", tab_idx, M.process(tab))
 
 		local is_last = tab_idx == #tabs
 		local next_tab = tabs[tab_idx + 1]
@@ -55,23 +75,20 @@ function M.setup(config)
 
 		if is_last then
 			arrow_fg = tab.is_active and active_bg or inactive_bg
-			arrow_bg = colors.tab_bar.background
 		elseif tab.is_active then
-			arrow_bg = inactive_bg
 			arrow_fg = active_bg
 		elseif next_is_active then
 			arrow_bg = active_bg
 			arrow_fg = inactive_bg
 		end
 
-		local ret = tab.is_active and {
-			{ Attribute = { Intensity = "Bold" } },
-		} or {}
-		ret[#ret + 1] = { Text = title }
-		ret[#ret + 1] = { Foreground = { Color = arrow_fg } }
-		ret[#ret + 1] = { Background = { Color = arrow_bg } }
-		ret[#ret + 1] = { Text = arrow }
-		return ret
+		return {
+			{ Attribute = { Intensity = tab.is_active and "Bold" or "Normal" } },
+			{ Text = title },
+			{ Foreground = { Color = arrow_fg } },
+			{ Background = { Color = arrow_bg } },
+			{ Text = arrow },
+		}
 	end)
 end
 
