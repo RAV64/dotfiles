d507b819483951a6774919a40b0eaa861909b3da
MikiS <73443709+RAV64@users.noreply.github.com>
2023-12-01 07:45:57 +0200

    On-call kindness embedded guided by empirical telemetry.

--- .hammerspoon/window-manager.lua
+++ .hammerspoon/window-manager.lua
@@ -1,16 +1,12 @@
 SCREEN = hs.screen.mainScreen()
-hs.grid.setGrid("8 * 4", SCREEN)
-
 GRID = hs.grid.getGrid(SCREEN)
 
-REGION = {
+local M = {
 	right_half = hs.geometry(0.5 * GRID.w, 0, 0.5 * GRID.w, GRID.h),
 	left_half = hs.geometry(0, 0, 0.5 * GRID.w, GRID.h),
 	top_half = hs.geometry(0, 0, GRID.w, 0.5 * GRID.h),
 	bottom_half = hs.geometry(0, 0.5 * GRID.h, GRID.w, 0.5 * GRID.h),
 }
-
-local M = {}
 
 M.maximizeWindow = function()
 	hs.grid.maximizeWindow(hs.window.focusedWindow())
@@ -20,20 +16,10 @@ M.centerOnScreen = function()
 	hs.window.focusedWindow():centerOnScreen(SCREEN)
 end
 
-M.leftHalf = function()
-	hs.grid.set(hs.window.focusedWindow(), REGION.left_half, SCREEN)
-end
-
-M.rightHalf = function()
-	hs.grid.set(hs.window.focusedWindow(), REGION.right_half, SCREEN)
-end
-
-M.topHalf = function()
-	hs.grid.set(hs.window.focusedWindow(), REGION.top_half, SCREEN)
-end
-
-M.bottomHalf = function()
-	hs.grid.set(hs.window.focusedWindow(), REGION.bottom_half, SCREEN)
+M.toHalf = function(region)
+	return function()
+		hs.grid.set(hs.window.focusedWindow(), region, SCREEN)
+	end
 end
 
 return M
