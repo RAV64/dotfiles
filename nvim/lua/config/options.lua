84a3b3f4b9e8b3be82a54fee11149adfe0fac905
MikiS <73443709+RAV64@users.noreply.github.com>
2024-01-22 08:47:03 +0200

    Stuff :)

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -30,7 +30,7 @@ opt.shortmess:append({
 	s = true, -- Don't show "Search hit BOTTOM" message
 })
 opt.showmode = false -- dont show mode since we have a statusline
-opt.sidescrolloff = 8 -- Columns of context
+opt.sidescrolloff = 0 -- Columns of context
 opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
 opt.smartcase = true -- Don't ignore case with capitals
 opt.smartindent = true -- Insert indents automatically
