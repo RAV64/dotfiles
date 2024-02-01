0fa8cc2c0d370586cfc9316b8b5c7b59f8c8be5c
MikiS <73443709+RAV64@users.noreply.github.com>
2024-02-01 04:39:58 +0200

    Principle of least surprise applied to satisfy pedantic linters.

--- fish/functions/update_neovim_nightly.fish
+++ fish/functions/update_neovim_nightly.fish
@@ -1,11 +1,13 @@
 function update_neovim_nightly
-   # Step 1: Download the file
-   curl -L -o ~/Downloads/nvim-macos.tar.gz https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz
+    # Step 1: Download the file
+    curl -L -o ~/Downloads/nvim-macos.tar.gz https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz
 
-   # Step 2: Change attributes to avoid "unknown developer" warning
-   xattr -c ~/Downloads/nvim-macos.tar.gz
+    # Step 2: Change attributes to avoid "unknown developer" warning
+    xattr -c ~/Downloads/nvim-macos.tar.gz
 
-   # Step 3: Extract the file
-   tar xzvf ~/Downloads/nvim-macos.tar.gz -C ~/dotfiles/scripts/bin
+    rm -rf ~/dotfiles/scripts/bin/nvim-macos
+
+    # Step 3: Extract the file
+    tar xzvf ~/Downloads/nvim-macos.tar.gz -C ~/dotfiles/scripts/bin
 
 end
