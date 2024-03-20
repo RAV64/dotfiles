0ee2621f6d38bdd5e74fe4198d5dad2a29b99186
MikiS <73443709+RAV64@users.noreply.github.com>
2024-03-20 09:39:46 +0200

    Compliance theater optimized to forestall another postmortem.

--- .hammerspoon/bluetooth.lua
+++ .hammerspoon/bluetooth.lua
@@ -1,29 +1,30 @@
 local dependency = hs.task.new("/opt/homebrew/bin/blueutil", nil, {}):start()
 
-if dependency then
-	function SetBluetooth(bool)
-		local state = bool and "on" or "off"
-		hs.task.new("/opt/homebrew/bin/blueutil", nil, { "--power", state }):start()
-	end
+if not dependency then
+	print("MISSING DEPENDENCY: blueutil (brew install blueutil)")
+	return
+end
 
-	function __AutoBluetooth(bool)
-		return function(_, stdOut, _)
-			if not bool then
-				STATE.auto_bluetooth = stdOut == "1\n"
-				if STATE.auto_bluetooth then
-					print("Bluetooth off")
-					SetBluetooth(bool)
-				end
-			elseif bool and STATE.auto_bluetooth then
-				print("Bluetooth on")
+function SetBluetooth(bool)
+	local state = bool and "on" or "off"
+	hs.task.new("/opt/homebrew/bin/blueutil", nil, { "--power", state }):start()
+end
+
+function __AutoBluetooth(bool)
+	return function(_, stdOut, _)
+		if not bool then
+			STATE.auto_bluetooth = stdOut == "1\n"
+			if STATE.auto_bluetooth then
+				print("Bluetooth off")
 				SetBluetooth(bool)
 			end
+		elseif bool and STATE.auto_bluetooth then
+			print("Bluetooth on")
+			SetBluetooth(bool)
 		end
-	end
-
-	return function(bool)
-		hs.task.new("/opt/homebrew/bin/blueutil", __AutoBluetooth(bool), { "-p" }):start()
 	end
 end
 
-print("MISSING DEPENDENCY: blueutil (brew install blueutil)")
+return function(bool)
+	hs.task.new("/opt/homebrew/bin/blueutil", __AutoBluetooth(bool), { "-p" }):start()
+end
