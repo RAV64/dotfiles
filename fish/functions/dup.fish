9bff6247b49756b67ae0b98bfd6437f6fb15dd4c
MikiS <73443709+RAV64@users.noreply.github.com>
2025-01-08 11:13:18 +0200

    update dist-upgrade

---
+++ fish/functions/dup.fish
@@ -0,0 +1,48 @@
+function dup
+    set os_name (uname)
+
+    switch $os_name
+        case "Darwin"
+            # macOS-specific commands
+            brew upgrade
+            brew upgrade wezterm@nightly
+            brew install --cask font-monaspace-nerd-font
+
+            bob use nightly
+
+            rustup update
+            cargo install-update -a
+
+            npm update -g
+
+            bun upgrade
+            bun-update
+
+            tldr --update
+
+        case "Linux"
+            if test -f /etc/os-release
+                set distro_name (awk -F= '/^ID=/{gsub(/"/,""); print $2}' /etc/os-release)
+
+                switch $distro_name
+                    case "opensuse-tumbleweed"
+                        sudo zypper ref
+                        sudo zypper dup --allow-vendor-change
+
+                    case "debian" "ubuntu"
+                        sudo apt update
+                        sudo apt full-upgrade
+                        sudo apt autoremove --purge
+                        sudo apt clean
+
+                    case '*'
+                        echo "This Linux distribution is not supported. Exiting."
+                end
+            else
+                echo "Cannot determine Linux distribution. Exiting."
+            end
+
+        case '*'
+            echo "Unsupported operating system: $os_name. Exiting."
+    end
+end
