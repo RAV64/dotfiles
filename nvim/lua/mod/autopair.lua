b848e4786c2adc1c80d56edd482064faa0c6bde5
MikiS <73443709+RAV64@users.noreply.github.com>
2025-05-06 07:07:32 +0300

    Throughput envelope expanded with deferential feature flags.

--- nvim/lua/mod/autopair.lua
+++ nvim/lua/mod/autopair.lua
@@ -1,56 +1,30 @@
-local pair_map = {
-	["("] = ")",
-	["["] = "]",
-	["{"] = "}",
-	["<"] = ">",
-	['"'] = '"',
-}
-
-local api = vim.api
-local keymap = vim.keymap.set
-
-local function termcodes(str)
-	return api.nvim_replace_termcodes(str, true, true, true)
+local cursor, line, set = vim.api.nvim_win_get_cursor, vim.api.nvim_get_current_line, vim.keymap.set
+local replace = function(s)
+	return vim.api.nvim_replace_termcodes(s, true, true, true)
 end
 
-local BS_SEQ = termcodes("<BS>")
-local DEL_SEQ = termcodes("<Del>")
-local UNDO_LFT = termcodes("<C-g>U<Left>")
-local SPREAD_PAIRS = termcodes("<Esc>O")
+local pairs_map = { ["("] = ")", ["["] = "]", ["{"] = "}", ["<"] = ">", ['"'] = '"' }
+local codes = { BS = replace("<BS>"), DEL = replace("<Del>"), U = replace("<C-g>U<Left>"), N = replace("<Esc>O") }
 
-local function get_col()
-	return api.nvim_win_get_cursor(0)[2]
-end
-
-local function char_at(col)
-	return api.nvim_get_current_line():sub(col, col)
-end
-
-local function should_pair()
-	local next_char = char_at(get_col() + 1)
-	return not next_char:match("%w")
-end
-
-for open, close in pairs(pair_map) do
-	keymap("i", open, function()
-		if char_at(get_col()) == "\\" or not should_pair() then
+for open, close in pairs(pairs_map) do
+	set("i", open, function()
+		local col, txt = cursor(0)[2] + 1, line()
+		if txt:sub(col - 1, col - 1) == "\\" or txt:sub(col, col):match("%w") then
 			return open
 		end
-		return open .. close .. UNDO_LFT
+		return open .. close .. codes.U
 	end, { expr = true, noremap = true, replace_keycodes = false })
 end
 
-local function skip_if_closer(key, skip_seq, default_seq)
-	keymap("i", key, function()
-		local col = get_col()
-		local cur = char_at(col)
-		local nxt = char_at(col + 1)
-		if pair_map[cur] == nxt then
-			return skip_seq()
+for key, act in pairs({
+	["<BS>"] = { codes.BS .. codes.DEL, "\b" },
+	["<CR>"] = { "\r" .. codes.N, "\r" },
+}) do
+	set("i", key, function()
+		local col, txt = cursor(0)[2] + 1, line()
+		if pairs_map[txt:sub(col - 1, col - 1)] == txt:sub(col, col) then
+			return act[1]
 		end
-		return default_seq
+		return act[2]
 	end, { expr = true, noremap = true, replace_keycodes = false })
 end
-
-skip_if_closer("<BS>", BS_SEQ .. DEL_SEQ, BS_SEQ)
-skip_if_closer("<CR>", "\r" .. SPREAD_PAIRS, "\r")
