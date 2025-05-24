372d68a005cf0d3e992e96451c6661b3be270d96
MikiS <73443709+RAV64@users.noreply.github.com>
2025-05-24 14:25:22 +0300

    Speculative-execution-proofed as a tribute to past regressions.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -73,15 +73,3 @@ end, { expr = true })
 
 -- https://docs.helix-editor.com/textobjects.html
 -- More helix maps?
-
-local jump_out = function()
-	local node_ok, node = pcall(vim.treesitter.get_node)
-	if not node_ok or not node then
-		vim.notify("TS not available")
-		return
-	end
-	local row, col = node:end_()
-	pcall(vim.api.nvim_win_set_cursor, 0, { row + 1, col })
-end
-
-vim.keymap.set({ "s", "i" }, "<Tab>", jump_out, { desc = "Smart tab (jump out)" })
