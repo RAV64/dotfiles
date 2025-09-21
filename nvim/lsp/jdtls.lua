369b20acccd8068a40f22f5d891b673848646702
MikiS <73443709+RAV64@users.noreply.github.com>
2025-09-21 12:24:49 +0300

    update lsps

--- nvim/lsp/jdtls.lua
+++ nvim/lsp/jdtls.lua
@@ -1,12 +1,14 @@
+--- [official downloads site](http://download.eclipse.org/jdtls/snapshots/?d)
+
 local java11 = "/opt/homebrew/opt/openjdk@11"
-local java21 = "/opt/homebrew/opt/openjdk@24"
+local java17 = "/opt/homebrew/opt/openjdk@17"
+local java21 = "/opt/homebrew/opt/openjdk@21"
 
 local runtimes = {
 	{ name = "JavaSE-11", path = java11, default = true },
+	{ name = "JavaSE-17", path = java17 },
 	{ name = "JavaSE-21", path = java21 },
 }
-
-local handlers = require("vim.lsp.handlers")
 
 local env = {
 	HOME = vim.uv.os_homedir(),
@@ -22,14 +24,6 @@ local function get_jdtls_cache_dir()
 	return get_cache_dir() .. "/jdtls"
 end
 
-local function get_jdtls_config_dir()
-	return get_jdtls_cache_dir() .. "/config"
-end
-
-local function get_jdtls_workspace_dir()
-	return get_jdtls_cache_dir() .. "/workspace"
-end
-
 local function get_jdtls_jvm_args()
 	local args = {}
 	for a in string.gmatch((env.JDTLS_JVM_ARGS or ""), "%S+") do
@@ -39,57 +33,44 @@ local function get_jdtls_jvm_args()
 	return unpack(args)
 end
 
--- TextDocument version is reported as 0, override with nil so that
--- the client doesn't think the document is newer and refuses to update
--- See: https://github.com/eclipse/eclipse.jdt.ls/issues/1695
-local function fix_zero_version(workspace_edit)
-	if workspace_edit and workspace_edit.documentChanges then
-		for _, change in pairs(workspace_edit.documentChanges) do
-			local text_document = change.textDocument
-			if text_document and text_document.version and text_document.version == 0 then
-				text_document.version = nil
-			end
-		end
-	end
-	return workspace_edit
+local root_markers = {
+	-- Multi-module projects
+	".git",
+	"build.gradle",
+	"build.gradle.kts",
+	-- Single-module projects
+	"build.xml", -- Ant
+	"pom.xml", -- Maven
+	"settings.gradle", -- Gradle
+	"settings.gradle.kts",
+}
+
+local function find_root()
+	local path = vim.fs.find(root_markers, { upward = true })[1]
+	return path and vim.fs.dirname(path) or vim.uv.cwd()
 end
 
-local function on_textdocument_codeaction(err, actions, ctx)
-	for _, action in ipairs(actions) do
-		-- TODO: (steelsojka) Handle more than one edit?
-		if action.command == "java.apply.workspaceEdit" then -- 'action' is Command in java format
-			action.edit = fix_zero_version(action.edit or action.arguments[1])
-		elseif type(action.command) == "table" and action.command.command == "java.apply.workspaceEdit" then -- 'action' is CodeAction in java format
-			action.edit = fix_zero_version(action.edit or action.command.arguments[1])
-		end
-	end
-
-	handlers[ctx.method](err, actions, ctx)
+-- Per-project workspace: <cache>/jdtls/workspace/<repo-name>-<short-hash>
+local function project_workspace_dir(root_dir)
+	local base = vim.fn.fnamemodify(root_dir, ":t")
+	local abs = vim.fn.fnamemodify(root_dir, ":p")
+	local hash = vim.fn.sha256(abs):sub(1, 8)
+	return table.concat({ get_jdtls_cache_dir() .. "/workspace", base .. "-" .. hash }, "/")
 end
 
-local function on_textdocument_rename(err, workspace_edit, ctx)
-	handlers[ctx.method](err, fix_zero_version(workspace_edit), ctx)
+local function ensure_dir(p)
+	vim.fn.mkdir(p, "p")
+	return p
 end
 
-local function on_workspace_applyedit(err, workspace_edit, ctx)
-	handlers[ctx.method](err, fix_zero_version(workspace_edit), ctx)
-end
-
--- Non-standard notification that can be used to display progress
-local function on_language_status(_, result)
-	local command = vim.api.nvim_command
-	command("echohl ModeMsg")
-	command(string.format('echo "%s"', result.message))
-	command("echohl None")
-end
+local workspace_dir = ensure_dir(project_workspace_dir(find_root()))
 
 return {
+  -- stylua: ignore
 	cmd = {
 		"jdtls",
-		"-configuration",
-		get_jdtls_config_dir(),
-		"-data",
-		get_jdtls_workspace_dir(),
+		"-configuration", get_jdtls_cache_dir() .. "/config",
+		"-data", workspace_dir,
 		get_jdtls_jvm_args(),
 	},
 	cmd_env = {
@@ -97,31 +78,30 @@ return {
 		PATH = java21 .. "/bin:" .. os.getenv("PATH"),
 	},
 	settings = {
-		java = { configuration = { runtimes = runtimes } },
+		java = {
+			-- eclipse = { downloadSources = true },
+			-- maven = { downloadSources = true },
+			configuration = { runtimes = runtimes },
+			compile = {
+				nullAnalysis = {
+					mode = "automatic",
+					nonnullbydefault = {},
+
+					nullable = {
+						"org.jetbrains.annotations.Nullable",
+					},
+					nonnull = {
+						"org.jetbrains.annotations.NotNull",
+					},
+				},
+			},
+		},
 	},
 	filetypes = { "java" },
-	root_markers = {
-		-- Multi-module projects
-		".git",
-		"build.gradle",
-		"build.gradle.kts",
-		-- Single-module projects
-		"build.xml", -- Ant
-		"pom.xml", -- Maven
-		"settings.gradle", -- Gradle
-		"settings.gradle.kts", -- Gradle
-	},
+	root_markers = root_markers,
 	init_options = {
-		workspace = get_jdtls_workspace_dir(),
+		workspace = workspace_dir,
 		jvm_args = {},
 		os_config = nil,
-	},
-	handlers = {
-		-- Due to an invalid protocol implementation in the jdtls we have to conform these to be spec compliant.
-		-- https://github.com/eclipse/eclipse.jdt.ls/issues/376
-		["textDocument/codeAction"] = on_textdocument_codeaction,
-		["textDocument/rename"] = on_textdocument_rename,
-		["workspace/applyEdit"] = on_workspace_applyedit,
-		["language/status"] = vim.schedule_wrap(on_language_status),
 	},
 }
