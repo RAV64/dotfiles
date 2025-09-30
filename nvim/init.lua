49314ef1330e0d751e0d6d700a37839b3b2d7db4
MikiS <73443709+RAV64@users.noreply.github.com>
2025-09-30 18:36:16 +0300

    Backwards-compatibly extended because we could not help ourselves.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,30 +1,38 @@
-_G.UTIL = require("config.util")
+local function r(module)
+	local status_ok, loaded_module = pcall(require, module)
+	if not status_ok then
+		vim.notify("Error loading " .. module, vim.log.levels.ERROR)
+		vim.notify(loaded_module, vim.log.levels.ERROR)
+	end
+	return loaded_module
+end
 
-require("config.options")
-require("config.plugins")
-require("vim._extui").enable({})
+_G.UTIL = r("config.util")
 
-vim.opt.clipboard = "unnamedplus"
-require("config.colors")
-require("config.keymaps")
-require("config.autocmds")
+r("config.options")
+r("config.plugins")
+r("vim._extui").enable({})
 
-require("plugin.misc")
-require("plugin.treesitter")
+r("config.colors")
+r("config.keymaps")
+r("config.autocmds")
 
-require("plugin.cmp")
-require("plugin.conform")
-require("plugin.gitsigns")
-require("plugin.grug-far")
-require("plugin.multicursor")
-require("plugin.picker")
+r("plugin.misc")
+r("plugin.treesitter")
 
-require("mod.statusline")
-require("mod.autopair")
-require("mod.puff-buff")
-require("mod.surround")
-require("mod.treesitter-matchit")
-require("mod.treesitter-diagnostics")
-require("mod.lsp")
-require("mod.quick-term")
-require("mod.smart-tab")
+r("plugin.cmp")
+r("plugin.conform")
+r("plugin.gitsigns")
+r("plugin.grug-far")
+r("plugin.multicursor")
+r("plugin.picker")
+
+r("mod.statusline")
+r("mod.autopair")
+r("mod.puff-buff")
+r("mod.surround")
+r("mod.treesitter-matchit")
+r("mod.treesitter-diagnostics")
+r("mod.lsp")
+r("mod.quick-term")
+r("mod.smart-tab")
