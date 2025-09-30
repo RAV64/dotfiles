242d4abadd6d67fd9764c55e892315443cf412b3
MikiS <73443709+RAV64@users.noreply.github.com>
2025-09-30 19:15:44 +0300

    Standards-complaint adjusted to satisfy pedantic linters.

--- nvim/lsp/rust-analyzer.lua
+++ nvim/lsp/rust-analyzer.lua
@@ -1,61 +1,33 @@
+-- https://rust-analyzer.github.io/book/configuration.html
+
+local cfg = {
+	cargo = { buildScripts = { enable = true } },
+	procMacro = { enable = true },
+	checkOnSave = true,
+	imports = {
+		granularity = { enforce = true, group = "module" },
+		merge = { glob = false },
+	},
+	check = { command = "clippy" },
+}
+
 return {
 	name = "rust-analyzer",
 	cmd = { "rust-analyzer" },
 	filetypes = { "rust" },
-	single_file_support = true,
 	root_dir = vim.fs.root(0, { "Cargo.toml", ".git" }),
 	capabilities = UTIL.capabilities({
 		{
 			experimental = {
-				commands = {
-					"rust-analyzer.runSingle",
-					"rust-analyzer.showReferences",
-					"rust-analyzer.gotoLocation",
-					-- "editor.action.triggerParameterHints",
-				},
-				codeActionGroup = false,
-				hoverActions = false,
-				hoverRange = false,
 				serverStatusNotification = true,
-				snippetTextEdit = false,
-				ssr = true,
-			},
-			textDocument = {
-				completion = {
-					completionItem = {
-						resolveSupport = {
-							properties = { "documentation", "detail", "additionalTextEdits" },
-						},
-					},
-				},
 			},
 		},
 	}),
-	init_options = {
-		-- cargo = { allFeatures = true },
-		runBuildScripts = { enable = true },
-		procMacro = { enable = true },
-		checkOnSave = true,
-		imports = {
-			granularity = {
-				enforce = true,
-				group = "module",
-			},
-			merge = {
-				glob = false,
-			},
-			preferNoStd = true,
-		},
-		check = {
-			command = "clippy",
-			-- features = "all",
-		},
-		provideFormatter = true,
+	settings = {
+		["rust-analyzer"] = cfg,
 	},
-	before_init = function(init_params, config)
+	before_init = function(init_params, _)
 		-- See https://github.com/rust-lang/rust-analyzer/blob/eb5da56d839ae0a9e9f50774fa3eb78eb0964550/docs/dev/lsp-extensions.md?plain=1#L26
-		if config.settings and config.settings["rust-analyzer"] then
-			init_params.initializationOptions = config.settings["rust-analyzer"]
-		end
+		init_params.initializationOptions = cfg
 	end,
 }
