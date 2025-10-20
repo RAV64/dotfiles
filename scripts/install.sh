ee1c98c019e738a281b253cf38c4c3726681747b
MikiS <73443709+RAV64@users.noreply.github.com>
2025-10-20 19:25:20 +0300

    Surgically excised as dictated by the runbooks.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,3 @@
-if ! ls ~/.local/share/fonts | grep -q '^JetBrains'; then
-	curl -fLo ../fonts/JetBrainsMono.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
-	unzip ../fonts/JetBrainsMono.zip -d ~/.local/share/fonts/
-fi
-
 if ! command -v fish >/dev/null 2>&1; then
 	sudo zypper --non-interactive install fish
 	chsh -s $(which fish)
@@ -19,7 +14,7 @@ fi
 if ! command -v rustc >/dev/null 2>&1; then
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 	rustup component add rust-analyzer
-	cargo install ripgrep fd-find bat starship zoxide exa git-delta
+	cargo install ripgrep fd-find bat starship zoxide exa difftastic
 fi
 
 if ! command -v fzf >/dev/null 2>&1; then
